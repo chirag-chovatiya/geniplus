2094c831dc9c9d469e88459c452972177bb8a1b2
chirag-chovatiya <134358879+chirag-chovatiya@users.noreply.github.com>
2025-05-09 23:30:35 +0530

    base url change

--- src/service/constant/api-constant.js
+++ src/service/constant/api-constant.js
@@ -1,5 +1,6 @@
 // export const BASE_URl = "http://localhost:3000/api/";
-export const BASE_URl = "https://lakshya-academy.vercel.app/api/";
+// export const BASE_URl = "https://lakshya-academy.vercel.app/api/";
+export const BASE_URl = "https://geniplus.vercel.app/api/";
 
 
 export const API = {
